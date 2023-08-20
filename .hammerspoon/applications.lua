49d8df08e81f39a0a471e999f178362072903531
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-08-20 01:11:19 -0700

    Fixed hammerspoon Apple Mail confusion with Proton Mail Bridge

--- .hammerspoon/applications.lua
+++ .hammerspoon/applications.lua
@@ -3,10 +3,13 @@ local function openOrFocusApplication(application, space)
 	local app = hs.application.get(application)
 	if not app then
 		yabai({ "-m", "space", "--focus", tostring(space) })
+		print("Focused space", space)
 		hs.application.open(application)
+		print("Opened " .. application)
 	else
 		-- focus the already open application
 		app:activate(true)
+		print("Focused", app)
 	end
 end
 
@@ -17,7 +20,8 @@ hs.hotkey.bind({ "alt" }, "d", function()
 	openOrFocusApplication("Discord", 5)
 end)
 hs.hotkey.bind({ "alt" }, "m", function()
-	openOrFocusApplication("Mail", 4)
+	-- Need to do this or else hammerspoon will get confused with Proton Mail Bridge
+	openOrFocusApplication("com.apple.mail", 4) -- Apple Mail
 end)
 hs.hotkey.bind({ "alt" }, "n", function()
 	openOrFocusApplication("Messages", 6)
@@ -26,5 +30,5 @@ hs.hotkey.bind({ "alt" }, "r", function()
 	openOrFocusApplication("Reminders", 6)
 end)
 hs.hotkey.bind({ "alt" }, "t", function()
-	openOrFocusApplication("iTerm2", 2)
+	openOrFocusApplication("iTerm", 2)
 end)
