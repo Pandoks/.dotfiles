43da63b3f2a0ccfda88e9058474d81016ce83f57
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-08-28 23:22:42 -0400

    Use bundle id for native apple apps

--- .hammerspoon/applications.lua
+++ .hammerspoon/applications.lua
@@ -20,14 +20,15 @@ hs.hotkey.bind({ "alt" }, "d", function()
 	openOrFocusApplication("Discord", 5)
 end)
 hs.hotkey.bind({ "alt" }, "m", function()
-	-- Need to do this or else hammerspoon will get confused with Proton Mail Bridge
-	openOrFocusApplication("com.apple.mail", 4) -- Apple Mail
+	-- For some reason, hammerspoon is more stable when using the bundle id for native apple apps
+	-- Can find the bundle if by doing: osascript -e 'id of app "<app>"'
+	openOrFocusApplication("com.apple.mail", 4)
 end)
 hs.hotkey.bind({ "alt" }, "n", function()
-	openOrFocusApplication("Messages", 6)
+	openOrFocusApplication("com.apple.MobileSMS", 6)
 end)
 hs.hotkey.bind({ "alt" }, "r", function()
-	openOrFocusApplication("Reminders", 6)
+	openOrFocusApplication("com.apple.reminders", 6)
 end)
 hs.hotkey.bind({ "alt" }, "t", function()
 	openOrFocusApplication("iTerm", 2)
