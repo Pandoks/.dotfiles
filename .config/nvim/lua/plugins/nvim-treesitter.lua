8c2990ab9e1650ac4322f7d9476d7df5f95ef20f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-09-18 18:11:19 -0700

    update: only use helm treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -15,27 +15,14 @@ return {
       "svelte",
       "comment",
       "yaml",
-      "gotmpl",
+      "helm",
     },
     auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      disable = { "yaml", "gotmpl" },
-    },
+    highlight = { enable = true },
+    indent = { enable = true, disable = { "yaml", "helm" } },
   },
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
-
-    if vim.treesitter.language and vim.treesitter.language.register then
-      vim.treesitter.language.register("gotmpl", "helm")
-    else
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.gotmpl = parser_config.gotmpl or {}
-      parser_config.gotmpl.filetype_to_parsername = { "helm", "gotmpl" }
-    end
 
     vim.filetype.add({
       pattern = { [".*/charts/.*/templates/.*%.ya?ml"] = "helm" },
