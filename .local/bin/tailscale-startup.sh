35b422b5ca1476ce9ea20d26aa424755d904b2a4
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-11 00:04:37 -0800

    update(tailscale): startup script

---
+++ .local/bin/tailscale-startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/opt/homebrew/bin/tailscale switch pandoks
+/usr/bin/sudo /bin/mkdir -p /etc/resolver
+echo "nameserver 100.100.100.100" | /usr/bin/sudo /usr/bin/tee /etc/resolver/ts.net >/dev/null
+/opt/homebrew/bin/tailscale up
