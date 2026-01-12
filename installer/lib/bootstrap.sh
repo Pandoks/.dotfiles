2eb7f9fe43591fd427067383ef83040cd777ee7d
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-11 16:18:57 -0800

    update(installer): omz

--- installer/lib/bootstrap.sh
+++ installer/lib/bootstrap.sh
@@ -3,4 +3,5 @@
 bootstrap() {
   xcode-select --install
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
