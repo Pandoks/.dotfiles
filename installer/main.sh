c12c4258401164d5383a5318d867558f6dfe26a5
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-11 18:43:06 -0800

    update(installer): no need eval after setup

--- installer/main.sh
+++ installer/main.sh
@@ -35,7 +35,6 @@ setup() {
   }
   RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
 }
 
 install_brew() {
