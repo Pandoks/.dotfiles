05326fec1b4837c3fb627caabb875d86abf56f1e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-02-15 23:13:32 -0800

    update(tailscale): no more custom tailscale needed

--- installer/lib/configs.sh
+++ installer/lib/configs.sh
@@ -16,9 +16,6 @@ install_configs() {
 
   create_symlink "${REPO_ROOT}/.hammerspoon" "${HOME}/.hammerspoon"
 
-  create_symlink "${REPO_ROOT}/.local/bin/tailscale-startup.sh" "${HOME}/.local/bin/tailscale-startup.sh"
-  create_symlink "${REPO_ROOT}/Library/LaunchAgents/com.tailscale.startup.plist" "${HOME}/Library/LaunchAgents/com.tailscale.startup.plist"
-
   create_symlink "${REPO_ROOT}/.fzf.zsh" "${HOME}/.fzf.zsh"
   create_symlink "${REPO_ROOT}/.fzfignore" "${HOME}/.fzfignore"
   create_symlink "${REPO_ROOT}/.gitconfig" "${HOME}/.gitconfig"
