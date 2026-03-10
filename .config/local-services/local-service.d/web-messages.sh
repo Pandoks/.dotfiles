c9e4f7b640707d372d6b83c25550ab2a8b76c3b1
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-03-10 01:00:46 -0700

    update(hosting): auto restart

--- .config/local-services/local-service.d/web-messages.sh
+++ .config/local-services/local-service.d/web-messages.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -eu
 
-exec ~/.local/bin/webmessages-v0.0.22 --port 55001
+trap 'exit 0' INT TERM HUP
+
+while :; do
+  ~/.local/bin/webmessages-v0.0.23 --port 55001
+  code=$?
+  echo "[web-messages] exited with code $code; restarting in 3s" >&2
+  sleep 3
+done
