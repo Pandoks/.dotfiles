21c2b521a28e4a2344a6b286d4b18d95f7f4b2b2
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-03-12 22:41:29 -0700

    update(hosting): updated web messages version

--- .config/local-services/local-service.d/web-messages.sh
+++ .config/local-services/local-service.d/web-messages.sh
@@ -4,7 +4,7 @@ set -eu
 trap 'exit 0' INT TERM HUP
 
 while :; do
-  ~/.local/bin/webmessages-v0.0.23 --port 55001
+  ~/.local/bin/webmessages-v0.0.25 --port 55001
   code=$?
   echo "[web-messages] exited with code $code; restarting in 3s" >&2
   sleep 3
