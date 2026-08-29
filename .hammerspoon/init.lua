820bc3a731e4ebfe768200fbaef1cec12ade194c
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-08-29 22:11:30 +0000

    update(mise): no more yabai

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,7 +2,6 @@ require("hs.ipc") -- `hs` CLI access for debugging
 hs.loadSpoon("EmmyLua") -- generates hs.* type annotations for lua_ls (only when stale)
 require("applications")
 require("ghostty")
-require("yabai")
 
 require("secrets")
 -- template for secrets:
